11779d1e85b5c3119869b589f480ce0b54e5045c
sumanbro0 <pandey.suman258@gmail.com>
2023-03-23 22:12:03 +0545

    added products

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, Cart, Product, OrderPlaced
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Cart)
+admin.site.register(Product)
+admin.site.register(OrderPlaced)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+
+STATE_CHOICES = (
+    ("1", "provience no. 1"),
+    ("2", "provience no. 2"),
+    ("3", "provience no. 3"),
+    ("4", "provience no. 4"),
+    ("5", "provience no. 5"),
+    ("6", "provience no. 6"),
+    ("7", "provience no. 7"),
+)
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    locality = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=50)
+
+    def __str__(self):
+        return str(self.id)
+
+
+CATEGORY_CHOICES = (
+    ("M", "Mobile"),
+    ("L", "Laptop"),
+    ("TW", "Top Wear"),
+    ("BW", "Bottom Wear"),
+)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    brand = models.CharField(max_length=255)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to="productimg")
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+
+STATUS_CHOICES = (
+    ("Accepted", "Accepted"),
+    ("Packed", "Packed"),
+    ("On The Way", "On The Way"),
+    ("Delivered", "Delivered"),
+    ("Cancel", "Cancel"),
+)
+
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="Pending")
